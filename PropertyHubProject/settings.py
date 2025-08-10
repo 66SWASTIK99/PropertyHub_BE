61729991c28ccb2af7f2b5232b733025c62cf2a6
swastik <swastik.079bei045@tcioe.edu.np>
2025-08-10 17:57:21 +0545

    render fix

--- PropertyHubProject/settings.py
+++ PropertyHubProject/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -167,4 +168,4 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 }
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'LAPTOP-8PD9MC2B']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
